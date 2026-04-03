567362e06408e2b041fcf84943b2b8ebcc027ec3
Scherman Lóránd <kkeksmaragd@gmail.com>
2026-04-03 11:08:02 +0200

    Add flymod class for client initialization and update mod entrypoints

---
+++ src/client/java/mods/example/flymod.java
@@ -0,0 +1,12 @@
+package mods.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class flymod implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        
+
+	}
+}
